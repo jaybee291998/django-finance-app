b0393b9ce549817614d97d5a2dd91f52a0ceb5cc
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2021-09-28 07:37:50 +0800

    updated template to be absolute for the server

--- finace/settings.py
+++ finace/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'finace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join('templates/')],
+        'DIRS': [os.path.join(.BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
