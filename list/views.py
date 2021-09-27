6ea5938aa53c8edfac410053adff7122265f7c8a
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2021-09-27 19:23:34 +0800

    implemented the remaining CRUD functionality
    just implemented the remaining CRUD functionality, hopefully it works. I made significant changes to facilitate the locations of list obj. The templates and urls are not yet done, so i wont know for now. The good thing is there is no error on the console so hoping for the best

--- list/views.py
+++ list/views.py
@@ -132,6 +132,8 @@ class ListEntryCreateView(CreateView):
 	fields = ('content',)
 
 	def form_valid(self, form):
+		# get the list_id then check if exist, if so check if the current user is the owner of the list
+		# object
 		list_id = self.kwargs['list_id']
 		try:
 			list_obj = List.objects.get(pk=list_id)
@@ -147,92 +149,163 @@ class ListEntryCreateView(CreateView):
 @method_decorator(login_required, name='dispatch')
 class ListEntryListView(ListView):
 	model = ListEntry
-	template_name = 'list/list.html'
-	context_object_name = 'lists'
+	template_name = 'list_entry/list.html'
+	context_object_name = 'list_entries'
 	paginate_by = 10
 
 	def get_queryset(self):
-		queryset = List.objects.filter(user=self.request.user).order_by('-timestamp')
+		list_obj = self.get_list_object()
+
+		queryset = ListEntry.objects.filter(list_obj=list_obj).order_by('-timestamp')
 		return queryset
 
 	def get_context_data(self, **kwargs):
-		context = super(ListListView, self).get_context_data(**kwargs)
-		lists = self.get_queryset()
+		list_obj = self.get_list_object()
+		context = super(ListEntryListView, self).get_context_data(**kwargs)
+		list_entries = self.get_queryset()
 		page = self.request.GET.get('page')
-		paginator = Paginator(lists, self.paginate_by)
+		paginator = Paginator(list_entries, self.paginate_by)
 
-		lists = context['lists']
-		detail_links = [reverse_lazy('list_detail', kwargs={'pk':list_obj.pk}) for list_obj in lists]
+		list_entries = context['list_entries']
+		detail_links = [reverse_lazy('list_entry_detail', kwargs={'pk':list_entry.pk, 'list_id':list_obj.id}) for list_entry in list_entries]
 		try:
-			lists = paginator.page(page)
+			list_entries = paginator.page(page)
 		except PageNotAnInteger:
-			lists = paginator.page(1)
+			list_entries = paginator.page(1)
 		except EmptyPage:
-			lists = paginator.page(paginator.num_pages)
-		context['list_details'] = zip(lists, detail_links)
-		context['add_list_link'] = reverse_lazy('list_create')
+			list_entries = paginator.page(paginator.num_pages)
+		context['list_entry_details'] = zip(list_entries, detail_links)
+		context['add_list_entry_link'] = reverse_lazy('list_entry_create')
 		context['go_home_link'] = reverse_lazy('home')
 		return context
 
+	def get_list_object(self):
+		# get the list_id then check if exist, if so check if the current user is the owner of the list
+		# object
+		list_id = self.kwargs['list_id']
+		try:
+			list_obj = List.objects.get(pk=list_id)
+		except LidtDoesNotExist:
+			raise Http404()
+		# check if the current user is the owner of the list object
+		if list_obj.user != self.request.user:
+			raise Http404()
+		return list_obj
+
+
 @method_decorator(login_required, name='dispatch')
-class ListDetailView(DetailView):
-	model = List
-	template_name = 'list/detail.html'
-	context_object_name = 'list'
+class ListEntryDetailView(DetailView):
+	model = ListEntry
+	template_name = 'list_entry/detail.html'
+	context_object_name = 'list_entry'
 
 	def get_object(self, queryset=None):
-		obj = super(ListDetailView, self).get_object(queryset=queryset)
-		if obj.user != self.request.user:
+		# get the current list object
+		list_obj = self.get_list_object()
+		obj = super(ListEntryDetailView, self).get_object(queryset=queryset)
+		# check if the current list entry object is not owned by the list obj
+		if obj.list_obj != list_obj:
 			raise Http404()
 		return obj
 
 	def get_queryset(self):
-		queryset = super(ListDetailView, self).get_queryset()
-		return queryset.filter(user=self.request.user)
+		list_obj = self.get_list_object()
+		queryset = super(ListEntryDetailView, self).get_queryset()
+		return queryset.filter(list_obj=list_obj)
 
 	def get_context_data(self, **kwargs):
-		context = super(ListDetailView, self).get_context_data(**kwargs)
-		list_obj = context['list']
-		delete_link = reverse_lazy('list_delete', kwargs={'pk':list_obj.pk})
-		update_link = reverse_lazy('list_update', kwargs={'pk':list_obj.pk})
+		list_obj = self.get_list_object()
+		context = super(ListEntryDetailView, self).get_context_data(**kwargs)
+		list_entry = context['list_entry']
+		delete_link = reverse_lazy('list_entry_delete', kwargs={'pk':list_entry.pk, 'list_id':list_obj.id})
+		update_link = reverse_lazy('list_update', kwargs={'pk':list_entry.pk})
 		context['delete_link'] = delete_link
 		context['update_link'] = update_link
-		context['go_back_link'] = reverse_lazy('lists_list')
+		context['go_back_link'] = reverse_lazy('list_entries_list')
 		return context
 
+	def get_list_object(self):
+		# get the list_id then check if exist, if so check if the current user is the owner of the list
+		# object
+		list_id = self.kwargs['list_id']
+		try:
+			list_obj = List.objects.get(pk=list_id)
+		except LidtDoesNotExist:
+			raise Http404()
+		# check if the current user is the owner of the list object
+		if list_obj.user != self.request.user:
+			raise Http404()
+		return list_obj
+
 
 @method_decorator(login_required, name='dispatch')
-class ListUpdateView(UpdateView):
-	model = List 
-	template_name = 'list/update.html'
-	context_object_name = 'list'
-	fields = ('title', 'description')
+class ListEntryUpdateView(UpdateView):
+	model = ListEntry 
+	template_name = 'list_entry/update.html'
+	context_object_name = 'list_entry'
+	fields = ('content',)
 
 	def get_success_url(self):
-		return reverse_lazy('list_detail', kwargs={'pk':self.object.id})
+		list_obj = self.get_list_object()
+		return reverse_lazy('list_entry_detail', kwargs={'pk':self.object.id, 'list_id':list_obj.id})
 
 	def get_object(self, queryset=None):
-		obj = super(ListUpdateView, self).get_object(queryset=queryset)
-		if obj.user != self.request.user:
+		list_obj = self.get_list_object()
+		obj = super(ListEntryUpdateView, self).get_object(queryset=queryset)
+		# check if the current list entry is owned by the list object
+		if obj.list_obj != list_obj:
 			raise Http404()
 		return obj
 
 	def get_queryset(self):
-		queryset = super(ListUpdateView, self).get_queryset()
-		return queryset.filter(user=self.request.user)
+		list_obj = self.get_list_object()
+		queryset = super(ListEntryUpdateView, self).get_queryset()
+		return queryset.filter(list_obj=list_obj)
+
+	def get_list_object(self):
+		# get the list_id then check if exist, if so check if the current user is the owner of the list
+		# object
+		list_id = self.kwargs['list_id']
+		try:
+			list_obj = List.objects.get(pk=list_id)
+		except LidtDoesNotExist:
+			raise Http404()
+		# check if the current user is the owner of the list object
+		if list_obj.user != self.request.user:
+			raise Http404()
+		return list_obj
 
 @method_decorator(login_required, name='dispatch')
-class ListDeleteView(DeleteView):
-	model = List
-	template_name = 'list/delete.html'
-	success_url = reverse_lazy('lists_list')
+class ListEntryDeleteView(DeleteView):
+	model = ListEntry
+	template_name = 'list_entry/delete.html'
+
+	def get_success_url(self):
+		list_obj = self.get_list_object()
+		return reverse_lazy('list_entry_detail', kwargs={'pk':self.object.id, 'list_id':list_obj.id})
 
 	def get_object(self, queryset=None):
+		list_obj = self.get_list_object()
 		obj = super(ListDeleteView, self).get_object(queryset=queryset)
-		if obj.user != self.request.user:
+		# check if the current list entry is owned by the list object
+		if obj.list_obj != list_obj:
 			raise Http404()
 		return obj
 
 	def get_queryset(self):
-		queryset = super(ListDeleteView, self).get_queryset()
-		return queryset.filter(user=self.request.user)
+		list_obj = self.get_list_object()
+		queryset = super(ListEntryDeleteView, self).get_queryset()
+		return queryset.filter(list_obj=list_obj)
+
+	def get_list_object(self):
+		# get the list_id then check if exist, if so check if the current user is the owner of the list
+		# object
+		list_id = self.kwargs['list_id']
+		try:
+			list_obj = List.objects.get(pk=list_id)
+		except LidtDoesNotExist:
+			raise Http404()
+		# check if the current user is the owner of the list object
+		if list_obj.user != self.request.user:
+			raise Http404()
+		return list_obj
