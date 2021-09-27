b0b215461dfd5147b4d48378db5612da44f5bd3f
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2021-09-27 19:32:03 +0800

    added list entry links
    added the list entry links, i have to make a few adhustments to accommodate the list id slug

--- list/urls.py
+++ list/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('detail/<int:pk>', views.ListDetailView.as_view(), name='list_detail'),
     path('update/<int:pk>', views.ListUpdateView.as_view(), name='list_update'),
     path('delete/<int:pk>', views.ListDeleteView.as_view(), name='list_delete'),
+    path('list_entry_list/<list_id>', views.ListEntryListView.as_view(), name='list_entries_list'),
     path('list_entry_create/<list_id>', views.ListEntryCreateView.as_view(), name='list_entry_create'),
+    path('list_entry_detail/<list_id>/<int:pk>', views.ListEntryDetailView.as_view(), name='list_entry_detail'),
+    path('list_entry_update/<list_id>/<int:pk>', views.ListEntryUpdateView.as_view(), name='list_entry_update'),
+    path('list_entry_delete/<list_id>/<int:pk>', views.ListEntryDeleteView.as_view(), name='list_entry_delete'),
+
 ]
