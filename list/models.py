ce2ee90668f90752671ded4c55c698cfe7ffb47f
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2021-09-27 17:18:26 +0800

    added list app
    added a new app, list is an app that records different kinds of list. Like shopping list, to do list, list of people that has debt to you or list of of people that you have debt. Its for general information records

---
+++ list/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+# Create your models here.
+class List(models.Model):
+	title 				= models.CharField(max_length=32)
+	description 		= models.TextField()
+	timestamp			= models.DateTimeField(auto_now_add=True)
+	user 				= models.ForeignKey(User, related_name='list', on_delete=models.CASCADE, null=True)
+
+	def __str__(self):
+		return f'{self.title} - {self.description[:15]}'
+
+class ListEntry(models.Model):
+	list_id				= models.ForeignKey(List, related_name='list_entries', on_delete=models.CASCADE, null=True)
+	content 			= models.TextField()
+	timestamp 			= models.DateTimeField(auto_now_add=True)
