daa2399f506656549591cb5d62911f4494ddfd06
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2021-09-29 13:48:28 +0800

    Update settings.py

--- finace/settings.py
+++ finace/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p^b1c@ih(oxeq9ntau1f-32ru-v)o&8srn2a_eo9d+(hcpo(^&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'jayvee291998.pythonanywhere.com']
 
