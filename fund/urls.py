d9b0f70dceb2a547aa9e66b117df48b9fe1661d0
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2021-10-30 17:44:12 +0800

    added fund allocation view to urls

--- fund/urls.py
+++ fund/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('create/', views.FundCreateView.as_view(), name='fund_create'),
     path('detail/<int:pk>', views.FundDetailView.as_view(), name='fund_detail'),
     path('update/<int:pk>', views.FundUpdateView.as_view(), name='fund_update'),
-    path('delete/<int:pk>', views.FundDeleteView.as_view(), name='fund_delete')
+    path('delete/<int:pk>', views.FundDeleteView.as_view(), name='fund_delete'),
+    path('fund_allocation/<int:fund_id>', views.fund_allocation_view, name='fund_allocation')
 ]
