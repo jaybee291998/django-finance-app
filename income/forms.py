e7a99aead62c560c57f236a60de3d77f09dd83e8
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2021-10-30 10:29:59 +0800

    Update forms.py

--- income/forms.py
+++ income/forms.py
@@ -24,12 +24,12 @@ class IncomeAddForm(forms.ModelForm):
 		if amount < 0:
 			raise ValidationError('Negative Integers are not allowed')
 
-		if self.prev_instance is not None:
-			prev_amount = self.prev_instance.amount
-			# if the prev_amount is reduced
-			if amount < prev_amount:
-				# if the amount reduced is greater than  the unallocated balance
-				if (prev_amount - amount) > self.bank_account.balance:
-					raise ValidationError(f'You cannot reduce this income by {prev_amount - amount}, because it is already in use')
+		# if self.prev_instance is not None:
+		# 	prev_amount = self.prev_instance.amount
+		# 	# if the prev_amount is reduced
+		# 	if amount < prev_amount:
+		# 		# if the amount reduced is greater than  the unallocated balance
+		# 		if (prev_amount - amount) > self.bank_account.balance:
+		# 			raise ValidationError(f'You cannot reduce this income by {prev_amount - amount}, because it is already in use')
 		
 		return amount
