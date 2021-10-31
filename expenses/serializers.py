231faec758b9362ebf251e7f59affb0a99c49e42
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2021-10-31 13:37:20 +0800

    Update serializers.py

--- expenses/serializers.py
+++ expenses/serializers.py
@@ -1,27 +1,10 @@
 from rest_framework import serializers
 from .models import Expense
 
-class ExpenseSerializer(serializers.Serializer):
-
-	CATEGORY_CHOICES = [('FD', 'FOOD'), ('SK', 'SNACKS'), ('CL', 'CLOTHING'), ('OS', 'ONLINE_SHOPPING')]
-
-	id 				= serializers.IntegerField(read_only=True)
-	description 	= serializers.CharField()
-	category	 	= serializers.ChoiceField(choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES[0][1])
-	price 			= serializers.IntegerField(default=0)
-	timestamp 		= serializers.DateTimeField()
-
-	def create(self, validated_data):
-		
-		return Expense.objects.create(**validated_data)
-
-	def update(self, instance, validated_data):
-
-		instance.description 	= validated_data.get('description', instance.description)
-		instance.category 		= validated_data.get('category', instance.category)
-		instance.price 			= validated_data.get('price', instance.price)
-		instance.save()
-		return instance
+class ExpenseSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Expense
+		fields = ['description', 'category', 'price', 'timestamp', 'fund', 'account']
 
 class ExpenseSerializer(serializers.ModelSerializer):
 	class Meta:
