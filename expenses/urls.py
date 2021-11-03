b6c8914431a124a034dab8f857f97c886556114c
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2021-11-03 18:42:52 +0800

    added the urls for the expensetypes

--- expenses/urls.py
+++ expenses/urls.py
@@ -7,6 +7,13 @@ urlpatterns = [
     path('detail/<int:pk>', views.ExpenseDetailView.as_view(), name='expenses_detail'),
     path('update/<int:pk>', views.ExpenseUpdateView.as_view(), name='expenses_update'),
     path('delete/<int:pk>', views.ExpenseDeleteView.as_view(), name='expenses_delete'),
+
+    path('et-list/', views.ExpenseTypeListView.as_view(), name='expense_types_list'),
+    path('et-create/', views.ExpenseTypeCreateView.as_view(), name='expense_type_create'),
+    path('et-detail/<int:pk>', views.ExpenseTypeDetailView.as_view(), name='expense_type_detail'),
+    path('et-update/<int:pk>', views.ExpenseTypeUpdateView.as_view(), name='expense_type_update'),
+    path('et-delete/<int:pk>', views.ExpenseTypeDeleteView.as_view(), name='expense_type_delete'),
+
     path('expenses_list_api/', views.ExpenseList.as_view(), name='expenses_list_api'),
     path('expenses_detail_api/<int:pk>', views.ExpenseDetail().as_view(), name='expenses_detail_api'),
     path('api_auth/', include('rest_framework.urls')),
