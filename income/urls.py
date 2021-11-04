13aeab28a8a8545089b097ac3dc575fe26bde00e
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2021-11-04 17:56:45 +0800

    added the links to incometypes

--- income/urls.py
+++ income/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path('create/', views.IncomeCreateView.as_view(), name='income_create'),
     path('detail/<int:pk>', views.IncomeDetailView.as_view(), name='income_detail'),
     path('update/<int:pk>', views.IncomeUpdateView.as_view(), name='income_update'),
-    path('delete/<int:pk>', views.IncomeDeleteView.as_view(), name='income_delete')
+    path('delete/<int:pk>', views.IncomeDeleteView.as_view(), name='income_delete'),
+
+    path('it-list/', views.IncomeTypeListView.as_view(), name='income_types_list'),
+    path('it-create/', views.IncomeTypeCreateView.as_view(), name='income_type_create'),
+    path('it-detail/<int:pk>', views.IncomeTypeDetailView.as_view(), name='income_type_detail'),
+    path('it-update/<int:pk>', views.IncomeTypeUpdateView.as_view(), name='income_type_update'),
+    path('it-delete/<int:pk>', views.IncomeTypeDeleteView.as_view(), name='income_type_delete')
 ]
