a9111e657671dd156c4f4d6c186647e316a0d305
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2021-11-10 16:55:48 +0800

    Update serializers.py

--- expenses/serializers.py
+++ expenses/serializers.py
@@ -4,4 +4,4 @@ from .models import Expense
 class ExpenseSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Expense
-		fields = ['description', 'category', 'price', 'timestamp', 'fund', 'account']
+		fields = ['id', 'description', 'category', 'price', 'timestamp', 'fund', 'account']
