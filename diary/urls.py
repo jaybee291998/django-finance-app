56925c0f20bf65e2332426f0d79226b71c4c80f7
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2021-11-10 17:11:39 +0800

    added the urls for the api views

--- diary/urls.py
+++ diary/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('create/', views.DiaryCreateView.as_view(), name='diary_create'),
     path('detail/<int:pk>', views.DiaryDetailView.as_view(), name='diary_detail'),
     path('update/<int:pk>', views.DiaryUpdateView.as_view(), name='diary_update'),
-    path('delete/<int:pk>', views.DiaryDeleteView.as_view(), name='diary_delete')
+    path('delete/<int:pk>', views.DiaryDeleteView.as_view(), name='diary_delete'),
+
+    path('diaries_list_api', views.DiaryList.as_view(), name='diaries-list-api'),
+    path('diary_detail_api', views.DiaryList.as_view(), name='diary-detail-api')
 ]
