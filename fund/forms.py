a805910a1c8c9349ddb0565d721e196e12ad2ebe
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-23 20:30:48 +0800

    added a form for fund transfer

--- fund/forms.py
+++ fund/forms.py
@@ -16,3 +16,20 @@ class FundAllocationForm(forms.Form):
 			raise ValidationError("Amount cant be negative")
 
 		return amount
+
+class FundTransferForm(forms.Form):
+	def __init__(self, account, current_fund, *args, **kwargs):
+		queryset = Fund.objects.filter(account=account).exclude(pk=current_fund.id)
+		super(FundTransferForm, self).__init__(*args, **kwargs)
+		self.fields['recipient_fund'].queryset = queryset
+
+	recipient_fund 		= forms.ModelChoiceField(queryset=None, initial=0)
+	amount 				= forms.IntegerField()
+
+	def clean_amount(self):
+		amount = self.cleaned_data.get('amount')
+
+		if amount > current_fund.amount:
+			raise ValidationError(f'You have insufficient amount to transfer.\nCurrent balance:{current_fund.amount}')
+
+		return amount
