f0bfffd070f3b1b022c261b42a43abb0a09eac1d
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-04-16 18:02:40 +0800

    forgot to import FundTransferHistory

--- fund/serializers.py
+++ fund/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Fund, FundAllocationHistory
+from .models import Fund, FundAllocationHistory, FundTransferHistory
 
 class FundSerializer(serializers.ModelSerializer):
     class Meta:
